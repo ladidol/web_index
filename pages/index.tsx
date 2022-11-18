fca5697632df53b9eaa77e0bef85064c0b4b8571
Zongzi <cs.zongzi@gmail.com>
2022-11-18 19:33:26 +0800

    feat: disable user scale

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Index() {
   return (
     <main className={styles.container}>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
         <title>CSZongzi</title>
         <meta name="description" content="This is CSZongzi's Homepage | Nice to meet you |•'-'•) ✧" />
       </Head>
